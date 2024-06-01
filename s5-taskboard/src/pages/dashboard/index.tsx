29e1415cbbe7fbe27739fe8da7133e778439f50d
Gabriel Koppe <Gabriel.koppe@gmail.com>
2024-06-01 14:18:26 -0300

    Section 5 - Criando dashboard

--- s5-taskboard/src/pages/dashboard/index.tsx
+++ s5-taskboard/src/pages/dashboard/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import styles from './styles.module.css';
 import { GetServerSideProps } from 'next';
 import { getSession } from 'next-auth/react';
+import TextArea from '@/component/header/textArea';
 
 export default function Dashboard() {
 	return (
@@ -10,7 +11,24 @@ export default function Dashboard() {
 				<title>Meu painel de tarefas</title>
 			</Head>
 
-			<h1>Página painel</h1>
+			<main className={styles.main}>
+				<section className={styles.content}>
+					<div className={styles.contentForm}>
+						<h1 className={styles.title}>Qual sua tarefa?</h1>
+						<form action="">
+							<TextArea placeholder="Digite qual a sua tarefa..." />
+							<div className={styles.checkboxArea}>
+								<input type="checkbox" className={styles.checkbox} />
+								<label>Deixar tarefa publica?</label>
+							</div>
+
+							<button type="submit" className={styles.button}>
+								Registrar
+							</button>
+						</form>
+					</div>
+				</section>
+			</main>
 		</div>
 	);
 }
