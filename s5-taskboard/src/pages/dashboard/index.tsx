4fa597b880b88a48a85e05907ea5b86811450628
Gabriel Koppe <Gabriel.koppe@gmail.com>
2024-06-02 02:04:08 -0300

    Section 5 - Listando Itens

--- s5-taskboard/src/pages/dashboard/index.tsx
+++ s5-taskboard/src/pages/dashboard/index.tsx
@@ -5,11 +5,18 @@ import { getSession } from 'next-auth/react';
 import TextArea from '@/components/textArea';
 import { FiShare2 } from 'react-icons/fi';
 import { FaTrash } from 'react-icons/fa';
-import { ChangeEvent, FormEvent, useState } from 'react';
+import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
 
 import { db } from '../../services/firebaseConnection';
 
-import { addDoc, collection } from 'firebase/firestore';
+import {
+	addDoc,
+	collection,
+	query,
+	orderBy,
+	onSnapshot,
+	where,
+} from 'firebase/firestore';
 
 interface DashboardProps {
 	user: {
@@ -17,9 +24,46 @@ interface DashboardProps {
 	};
 }
 
+interface TaskType {
+	id: string;
+	created: Date;
+	public: boolean;
+	tarefa: string;
+	user: string;
+}
+
 export default function Dashboard({ user }: DashboardProps) {
 	const [input, setInput] = useState('');
 	const [publicTask, setPublicTask] = useState(false);
+	const [tasks, setTasks] = useState<TaskType[]>([]);
+
+	useEffect(() => {
+		const loadTarefas = async () => {
+			const tarefasRef = collection(db, 'tarefas');
+			const q = query(
+				tarefasRef,
+				orderBy('created', 'desc'),
+				where('user', '==', user.email),
+			);
+			onSnapshot(q, (snapshot) => {
+				let lista = [] as TaskType[];
+
+				snapshot.forEach((doc) => {
+					lista.push({
+						id: doc.id,
+						created: doc.data().created,
+						public: doc.data().public,
+						tarefa: doc.data().tarefa,
+						user: doc.data().user,
+					});
+				});
+
+				setTasks(lista);
+			});
+		};
+
+		loadTarefas();
+	}, [user.email]);
 
 	const handleChangePublic = (e: ChangeEvent<HTMLInputElement>) => {
 		setPublicTask(e.target.checked);
@@ -81,20 +125,24 @@ export default function Dashboard({ user }: DashboardProps) {
 				</section>
 				<section className={styles.taskContainer}>
 					<h1>Minhas Tarefas</h1>
-					<article className={styles.task}>
-						<div className={styles.tagContainer}>
-							<label className={styles.tag}>PUBLICO</label>
-							<button className={styles.shareButton}>
-								<FiShare2 size={22} color="#3183ff" />
-							</button>
-						</div>
-						<div className={styles.taskContent}>
-							<p>Minha primeira tarefa</p>
-							<button className={styles.trashButton}>
-								<FaTrash size={24} color="#ea3140" />
-							</button>
-						</div>
-					</article>
+					{tasks.map((item) => (
+						<article key={item.id} className={styles.task}>
+							{item.public && (
+								<div className={styles.tagContainer}>
+									<label className={styles.tag}>PUBLICO</label>
+									<button className={styles.shareButton}>
+										<FiShare2 size={22} color="#3183ff" />
+									</button>
+								</div>
+							)}
+							<div className={styles.taskContent}>
+								<p>{item.tarefa}</p>
+								<button className={styles.trashButton}>
+									<FaTrash size={24} color="#ea3140" />
+								</button>
+							</div>
+						</article>
+					))}
 				</section>
 			</main>
 		</div>
