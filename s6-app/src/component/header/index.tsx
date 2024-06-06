9ec54bc2d31085e968f734fa2375a4c0df120321
Gabriel Koppe <Gabriel.koppe@gmail.com>
2024-06-06 00:32:23 -0300

    Section 6 - Conhecendo loading

---
+++ s6-app/src/component/header/index.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+import styles from './header.module.css';
+
+export default function Header() {
+	return (
+		<header className={styles.header}>
+			<h1>Meu Site</h1>
+			<Link href="/">Home</Link>
+			<br />
+			<Link href="/contatos">Contatos</Link>
+			<br />
+			<hr />
+		</header>
+	);
+}
