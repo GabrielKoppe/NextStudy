408c96ea8a46a8bf2fe92a29ea772e2b81029995
Gabriel Koppe <Gabriel.koppe@gmail.com>
2024-06-10 20:42:55 -0300

    Section 7 - Criando header

--- s7-dailygames/src/components/header/index.tsx
+++ s7-dailygames/src/components/header/index.tsx
@@ -1,7 +1,32 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import { LiaGamepadSolid } from 'react-icons/lia';
+
 export default function Header() {
 	return (
 		<header className="w-full h-28 bg-slate-100 text-black px-2">
-			<h1>Header Teste</h1>
+			<div className="max-w-screen-xl mx-auto flex justify-center items-center h-28 sm:justify-between">
+				<nav className="flex justify-center items-center gap-4">
+					<Link href="/">
+						<Image
+							src="/logo.svg"
+							alt="Logo site Dailygames"
+							quality={100}
+							width={150}
+							height={150}
+							priority
+							className="w-full"
+						/>
+					</Link>
+					<Link href="/">Games</Link>
+					<Link href="/profile">Perfil</Link>
+				</nav>
+				<div className="hidden sm:flex justify-center items-center">
+					<Link href="/profile">
+						<LiaGamepadSolid size={34} color="#475569" />
+					</Link>
+				</div>
+			</div>
 		</header>
 	);
 }
