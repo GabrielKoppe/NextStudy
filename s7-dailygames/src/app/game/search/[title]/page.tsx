fc045fae8af687dec2fa5cc5c88171b14837f91e
Gabriel Koppe <Gabriel.koppe@gmail.com>
2024-06-10 23:19:02 -0300

    Section 7 - Ajustando Metadata

--- s7-dailygames/src/app/game/search/[title]/page.tsx
+++ s7-dailygames/src/app/game/search/[title]/page.tsx
@@ -5,8 +5,9 @@ import { GameProps } from '@/utils/types/game';
 
 async function getGames(title: string): Promise<GameProps[] | null> {
 	try {
+		const decodeTitle = decodeURI(title);
 		const res = await fetch(
-			`${process.env.NEXT_API_URL}/next-api/?api=game&title=${title}`,
+			`${process.env.NEXT_API_URL}/next-api/?api=game&title=${decodeTitle}`,
 			{ next: { revalidate: 320 } },
 		);
 		return res.json();
