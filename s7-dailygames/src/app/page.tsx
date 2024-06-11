76c8a0bc038cbde4fae5595510f2a5ac27037bb9
Gabriel Koppe <Gabriel.koppe@gmail.com>
2024-06-10 21:17:32 -0300

    Section 7 - Criando Jogo do dia

--- s7-dailygames/src/app/page.tsx
+++ s7-dailygames/src/app/page.tsx
@@ -2,11 +2,13 @@ import Container from '@/components/container';
 import { GameProps } from '@/utils/types/game';
 import Link from 'next/link';
 import Image from 'next/image';
+import { BsArrowRightSquare } from 'react-icons/bs';
 
 async function getDailyGame(): Promise<GameProps> {
 	try {
 		const res = await fetch(
 			`${process.env.NEXT_API_URL}/next-api/?api=game_day`,
+			{ next: { revalidate: 320 } },
 		);
 		return res.json();
 	} catch (error) {
@@ -25,14 +27,23 @@ export default async function Home() {
 				</h1>
 				<Link href={`/game/${dailyGame.id}`}>
 					<section className="w-full bg-black rounded-lg">
-						<Image
-							src={dailyGame.image_url}
-							alt={dailyGame.title}
-							priority
-							quality={100}
-							width={100}
-							height={100}
-						/>
+						<div className="w-full max-h-96 h-96 relative rounded-lg">
+							<div className="absolute z-20 bottom-0 p-3 flex justify-center items-center gap-2">
+								<p className="font-bold  text-xl text-white">
+									{dailyGame.title}
+								</p>
+								<BsArrowRightSquare size={24} color="#fff" />
+							</div>
+							<Image
+								src={dailyGame.image_url}
+								alt={dailyGame.title}
+								priority
+								quality={100}
+								fill
+								className="max-h-96 object-cover rounded-lg opacity-50 hover:opacity-100 transition-all duration-300"
+								sizes="(max-width:: 760px) 100vw, (max-width:: 1200px) 44vw"
+							/>
+						</div>
 					</section>
 				</Link>
 			</Container>
