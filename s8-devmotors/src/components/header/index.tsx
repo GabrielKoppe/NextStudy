6cc8dd828876965ec362f6eda85df1aad266cd28
Gabriel Koppe <Gabriel.koppe@gmail.com>
2024-06-15 16:38:25 -0300

    Section 8 - Criando header

--- s8-devmotors/src/components/header/index.tsx
+++ s8-devmotors/src/components/header/index.tsx
@@ -1,9 +1,39 @@
+'use client';
+
+import Link from 'next/link';
 import styles from './styles.module.scss';
+import { useEffect, useState } from 'react';
 
 export default function Header() {
+	const [top, setTop] = useState(true);
+
+	const handlerScroll = () => {
+		const isOnTop = !(window.scrollY > 10);
+		setTop(isOnTop);
+	};
+
+	useEffect(() => {
+		window.addEventListener('scroll', handlerScroll);
+
+		return () => window.removeEventListener('scroll', handlerScroll);
+	}, [top]);
+
 	return (
-		<header>
-			<h1>Header</h1>
+		<header
+			className={`${styles.header} ${top ? styles.background : styles.fixed}`}
+		>
+			<div className={styles.container}>
+				<div className={styles.content}>
+					<div className={styles.logo}>
+						<Link href="/">Dev Motors</Link>
+					</div>
+					<nav>
+						<Link href="/">HOME</Link>
+						<Link href="/#servicos">SERVIÇOS</Link>
+						<Link href="/#contatos">CONTATOS</Link>
+					</nav>
+				</div>
+			</div>
 		</header>
 	);
 }
