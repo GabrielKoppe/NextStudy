0ca231e6a96cb25963d7e11a25ed40d1ff9f7561
Gabriel Koppe <Gabriel.koppe@gmail.com>
2024-06-16 13:00:31 -0300

    Section 8 - Estilizando Submenu

--- s8-devmotors/src/components/homeheader/index.tsx
+++ s8-devmotors/src/components/homeheader/index.tsx
@@ -1,20 +1,47 @@
+'use client';
+
 import Link from 'next/link';
 import styles from './styles.module.scss';
-import { Menu } from 'lucide-react';
+
+import { Menu, X } from 'lucide-react';
+import { useEffect, useState } from 'react';
 
 export default function HomeHeader() {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const handleResize = () => {
+		if (window.innerWidth > 768) {
+			setIsOpen(false);
+		}
+	};
+
+	useEffect(() => {
+		window.addEventListener('resize', handleResize);
+
+		return () => window.removeEventListener('resize', handleResize);
+	}, [isOpen]);
+
+	function toggleMenu() {
+		setIsOpen(!isOpen);
+	}
 	return (
 		<section className={styles.submenu}>
-			<div className={styles.submenuIcon}>
+			<div className={styles.submenuIcon} onClick={toggleMenu}>
 				<Menu size={34} color="#121212" />
-				Menu
+				Serviços
 			</div>
-			<ul className={styles.ul}>
+
+			<ul className={`${styles.ul} ${isOpen ? styles.open : ''}`}>
+				{isOpen && (
+					<button onClick={toggleMenu} className={styles.closeButton}>
+						<X size={54} color="#121212" />
+					</button>
+				)}
 				<li>
-					<Link href="/">Pagina 1</Link>
+					<Link href="/post/pagina-1">Pagina 1</Link>
 				</li>
 				<li>
-					<Link href="/">Pagina 2</Link>
+					<Link href="/post/pagina-2">Pagina 2</Link>
 				</li>
 			</ul>
 		</section>
