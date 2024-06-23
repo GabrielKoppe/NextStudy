1d6b384b373bf29227c1ab735900550f50358821
Gabriel Koppe <Gabriel.koppe@gmail.com>
2024-06-23 15:11:49 -0300

    Section 9 - Criando projeto e header

---
+++ s9-devcontrole/src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import Header from '@/components/header';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+	title: 'Dev Controle - Seu sistema de gerenciamento',
+	description: 'Gerencie seus clientes e atendimento de forma fácil',
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en">
+			<body className={inter.className}>
+				<Header />
+				{children}
+			</body>
+		</html>
+	);
+}
