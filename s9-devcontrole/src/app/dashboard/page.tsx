9f0853d4c7c29dc09b07da523b9b7031ea092d34
Gabriel Koppe <Gabriel.koppe@gmail.com>
2024-06-26 18:25:25 -0300

    Section 9 - Criando Tabela de Tickets

--- s9-devcontrole/src/app/dashboard/page.tsx
+++ s9-devcontrole/src/app/dashboard/page.tsx
@@ -1,7 +1,36 @@
+import Link from 'next/link';
+import TicketItem from './components/ticket';
+
 export default async function Dashboard() {
 	return (
-		<>
-			<h1>Teste</h1>
-		</>
+		<main className="mt-9 mb-2">
+			<div className="flex justify-between items-center">
+				<h1 className="text-3xl font-bold">Chamados</h1>
+				<Link
+					href="/dashboard/new"
+					className="bg-blue-500 px-4 py-1 rounded text-white hover:bg-blue-600 duration-300"
+				>
+					Abrir Chamado
+				</Link>
+			</div>
+
+			<table className="min-w-full my-2">
+				<thead>
+					<tr>
+						<th className="font-medium text-left uppercase pl-1">Cliente</th>
+						<th className="font-medium text-left uppercase hidden sm:block">
+							Data Cadastro
+						</th>
+						<th className="font-medium text-left uppercase">Status</th>
+						<th className="font-medium text-left uppercase">#</th>
+					</tr>
+				</thead>
+				<tbody>
+					<TicketItem />
+					<TicketItem />
+					<TicketItem />
+				</tbody>
+			</table>
+		</main>
 	);
 }
